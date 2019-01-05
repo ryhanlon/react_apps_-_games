a6a96407365b7fd112431834a07c8bd3a5546bf6
ryhanlon <ryhanlon@gmail.com>
2019-01-04 17:06:06 -0800

    feat: Add Styling for footer, remove redundant role

--- tempconverter/src/components/App/App.js
+++ tempconverter/src/components/App/App.js
@@ -33,13 +33,13 @@ class App extends Component {
         }
 
         if (this.fahrenheit > 500) {
-            this.message = <div><p className="message-text">Solar sailing!</p><img className="message-picture" role="img" tabIndex="0" src={hot} alt='Surface of the sun'/></div>;
+            this.message = <div><p className="message-text">Solar sailing!</p><img className="message-picture" tabIndex="0" src={hot} alt='Surface of the sun'/></div>;
         } else if(this.fahrenheit > 200) {
-            this.message = <div><p className="message-text">Catch a wave!</p><img className="message-picture" role="img" tabIndex="0" src={sunny} alt='Surfing a large wave'/></div>;
+            this.message = <div><p className="message-text">Catch a wave!</p><img className="message-picture" tabIndex="0" src={sunny} alt='Surfing a large wave'/></div>;
         } else if(this.fahrenheit > 50) {
-            this.message = <div><p className="message-text">Hiking time!</p><img className="message-picture" role="img" tabIndex="0" src={cool} alt='A hiking path'/></div>;
+            this.message = <div><p className="message-text">Hiking time!</p><img className="message-picture" tabIndex="0" src={cool} alt='A hiking path'/></div>;
         } else {
-            this.message =  <div><p className="message-text">Glacier glide!</p><img className="message-picture" role="img" tabIndex="0"  src={cold} alt='Penguin sliding on ice'/></div>;
+            this.message =  <div><p className="message-text">Glacier glide!</p><img className="message-picture" tabIndex="0"  src={cold} alt='Penguin sliding on ice'/></div>;
         }
 
         this.setState({
